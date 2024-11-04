9cedda255d7d55a95eff37a1c4c2e6e4e7bb1670
Vishant <iamvishant01@gmail.com>
2024-11-04 13:19:06 +0530

    MeetNest

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ io.on("connection", (socket) => {
   socket.on("call:accepted", ({ to, ans }) => {
     io.to(to).emit("call:accepted", { from: socket.id, ans });
   });
-
+// iamvishant01
   socket.on("peer:nego:needed", ({ to, offer }) => {
     console.log("peer:nego:needed", offer);
     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
