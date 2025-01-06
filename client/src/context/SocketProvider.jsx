fb2ac07c13f114678dd7a26eabeaa9f06bd3335c
Vishant <iamvishant01@gmail.com>
2025-01-06 22:56:24 +0530

    localhost address changed

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -1,6 +1,9 @@
 import React, { createContext, useMemo, useContext } from "react";
 import { io } from "socket.io-client";
 
+// Fetch the backend URL from environment variables
+const backendURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
+
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
@@ -9,7 +12,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io(backendURL), []);
 
   return (
     <SocketContext.Provider value={socket}>
