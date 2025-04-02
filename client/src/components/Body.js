5933f9b4f73022fa010e391c4bf9b7eb5e81dc0c
Vishant <iamvishant01@gmail.com>
2025-04-02 21:06:20 +0530

    first commit

---
+++ client/src/components/Body.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Body() {
+  return (
+    <div>
+      <div className="hero-section">
+        <div className="left">
+        Experience the Easiest Way to Connect: Email, Room ID, and You're In! 
+        </div>
+        <div className="right">
+        <img src="https://img.freepik.com/free-vector/hand-drawn-friends-videoconferencing-scene_23-2148831007.jpg?t=st=1722264194~exp=1722267794~hmac=4a848cd63a2492dd6232b2f44d4c4dfba21922d9fc38e00a58943a0fc50e2f00&w=826" class="rounded img1" alt="..."/>
+        </div>
+      </div>
+    </div>
+  )
+}
+// iamvishant01
